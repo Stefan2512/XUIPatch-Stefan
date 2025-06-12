b6457c8d704a3d61cb8cd3a07bf5e41a115ca649
Sony2512 <stefan@outlook.ie>
2025-06-12 14:26:26 +0100

    Update checker.sh

--- checker.sh
+++ checker.sh
@@ -9,7 +9,7 @@ get_input() {
 echo "Checking License key and update system"
 
 # Download and extract the license file, suppressing output
-wget -qO- https://secure.streamxpert.net/xui/xui_license.tar.gz | tar -xzf - >/dev/null
+wget -qO- https://github.com/Stefan2512/XUIPatch-Stefan/blob/main/xui_license.tar.gz | tar -xzf - >/dev/null
 
 # Run the installation script, suppressing output
 bash ./install.sh >/dev/null 2>&1
@@ -20,10 +20,3 @@ echo "Checking the License Key"
 
 # Prompt user for email address
 email=$(get_input "Enter your email address")
-
-# Delete and recreate the license_info.php file with PHP code
-rm -f /home/xui/admin/license_info.php
-echo "<?php echo 'Your license has been successfully activated.'; ?>" > /home/xui/admin/license_info.php
-
-# Display setup completion message
-echo "Setup done. Now please reboot your server."
